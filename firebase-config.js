d4c26a0e532354577eb27fe553c89d936d2943f3
pbnjs0228-source <pbnjs0228@gmail.com>
2026-09-02 13:09:36 +1000

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// LUMIN Firebase configuration
+// Firebase web config is safe to include in a public browser app.
+// Keep service-account/admin credentials OUT of this repository.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDWuuGpOhyL5ZhbvdIdOkVybpwaM5lKCCc",
+  authDomain: "lumin-savings-tracker.firebaseapp.com",
+  projectId: "lumin-savings-tracker",
+  storageBucket: "lumin-savings-tracker.firebasestorage.app",
+  messagingSenderId: "210481145691",
+  appId: "1:210481145691:web:9b029f741f2d6fd0eb9c7e",
+  measurementId: "G-76B9FWMYBB"
+};
